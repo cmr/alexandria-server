bac79c51fac9643d840d98b532e98b508af251ce
Corey Richardson <corey@octayn.net>
2014-10-12 16:38:00 -0400

    Simple skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+extern crate iron;
+extern crate router;
+
+use std::io::net::ip::Ipv4Addr;
+use iron::{Iron, status, Request, Response, IronResult};
+use router::{Router, Params};
+
+fn get_book(req: &mut Request) -> IronResult<Response> {
+    Ok(match req.extensions.find::<Router, Params>().unwrap().find("id") {
+        Some(id) => {
+            Response::with(status::Ok, format!(r#"{{"success":true, "got_id": {}}}"#, id))
+        },
+        None => Response::status(status::BadRequest)
+    })
+}
+
+
 fn main() {
-    println!("Hello, world!")
+    let mut router = Router::new();
+    router.get("/book/:id", get_book);
+
+    Iron::new(router).listen(Ipv4Addr(127, 0, 0, 1), 13699);
 }
