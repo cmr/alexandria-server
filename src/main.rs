b063f1fc70684464f3db427438d310c4eb70b610
Alexander Macri <admfav2012@gmail.com>
2014-10-20 17:22:03 -0400

    added stuff

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ extern crate persistent;
 extern crate postgres;
 extern crate router;
 extern crate serialize;
+extern crate bodyparser;
 
 use std::io::net::ip::Ipv4Addr;
 use serialize::json;
@@ -12,6 +13,7 @@ use iron::{ChainBuilder, Chain, Iron, status, Request, Response, IronResult, Plu
 use persistent::{Write};
 use postgres::{PostgresConnection, NoSsl};
 use router::{Router, Params};
+use bodyparser::BodyParser;
 
 struct DBConn;
 impl typemap::Assoc<PostgresConnection> for DBConn { }
@@ -82,8 +84,8 @@ fn get_book_by_isbn(req: &mut Request) -> IronResult<Response> {
     Ok(match req.extensions.find::<Router, Params>().unwrap().find("isbn") {
         Some(isbn) => {
             let conn = conn.lock();
-            let stmt = conn.prepare("SELECT * FROM books WHERE isbn = $1").unwrap();
-            for row in stmt.query(&[&String::from_str(isbn)]).unwrap() {
+            let stmt = conn.prepare("SELECT * FROM books WHERE isbn=$2").unwrap();
+            for row in stmt.query([&String::from_str(isbn)]).unwrap() {
                 let book = book_from_row(row);
                 return Ok(good(&book))
             }
@@ -97,11 +99,12 @@ fn get_book_by_isbn(req: &mut Request) -> IronResult<Response> {
 //update book from request
 fn update_book_by_isbn(req: &mut Request) -> IronResult<Response> {
     let conn = req.get::<Write<DBConn, PostgresConnection>>().unwrap();
-    Ok(match req.extensions.find::<Router, Params>().unwrap().find("book") {
-        Some(book) => {
+    Ok(match req.extensions.find::<Router, Params>().unwrap().find("isbn") {
+        Some(isbn) => {
             let conn = conn.lock();
-            let stmt = conn.prepare("UPDATE books SET book=$1,book=$2,isbn=$3,book=$4,book=$5,book=$6,book=$7,book=$8 WHERE book=$9").unwrap();
-            match stmt.execute(&[&String::from_str(book),&String::from_str(book),&String::from_str(book),&book,&book,&book,&book,&book,&book]) {
+            let stmt = conn.prepare("UPDATE books SET name=$1,description=$2,isbn=$3,cover_image=$4,available=$5,quantity=$6,active_date=$7,permission=$8 WHERE isbn=$8").unwrap();
+            req.get::<BodyParser>();
+            match stmt.execute(&[&String::from_str(name),&String::from_str(description),&cover_image,&available,&quantity,&active_date,&permission,&isbn]) {
     					Ok(num) => println!("Update Book! {}", num),
     					Err(err) => println!("Error executing update_book_by_isbn: {}", err)
 						}
@@ -117,8 +120,9 @@ fn add_book_by_isbn(req: &mut Request) -> IronResult<Response> {
     Ok(match req.extensions.find::<Router, Params>().unwrap().find("book") {
         Some(book) => {
             let conn = conn.lock();
-            let stmt = conn.prepare("INSERT INTO books VALUES (book=$1,book=$2,book=$3,book=$4,book=$5,book=$6,book=$7,book=$8").unwrap();
-            match stmt.execute(&[&String::from_str(book),&String::from_str(book),&String::from_str(book),&book,&book,&book,&book,&book]) {
+            let stmt = conn.prepare("INSERT INTO books (name,description,isbn,cover_image,available,quantity,active_date,permission) VALUES ($1,$2,$3,$4,$5,$6,$7,$8").unwrap();
+            
+            match stmt.execute(&[&String::from_str(book.name),&String::from_str(book.description),&book.cover_image,&book.available,&book.quantity,&book.active_date,&book.permission]) {
     					Ok(num) => println!("Added Book! {}", num),
     					Err(err) => println!("Error executing add_book_by_isbn: {}", err)
 						}
@@ -131,11 +135,12 @@ fn add_book_by_isbn(req: &mut Request) -> IronResult<Response> {
 //delete book from request
 fn delete_book_by_isbn(req: &mut Request) -> IronResult<Response> {
     let conn = req.get::<Write<DBConn, PostgresConnection>>().unwrap();
-    Ok(match req.extensions.find::<Router, Params>().unwrap().find("isbn") {
-        Some(isbn) => {
+    Ok(match req.extensions.find::<Router, Params>().unwrap().find("Book") {
+        Some(Book) => {
             let conn = conn.lock();
-            let stmt = conn.prepare("DELETE FROM books WHERE isbn = $1").unwrap();
-            match stmt.execute(&[&String::from_str(isbn)]) {
+            let stmt = conn.prepare("DELETE FROM books WHERE isbn=$1 OR name=$2").unwrap();
+            
+            match stmt.execute(&[&String::from_str(Book.isbn),&String::from_str(Book.name)]) {
     					Ok(num) => println!("Deleted Book! {}", num),
     					Err(err) => println!("Error executing delete_book_by_isbn: {}", err)
 						}
@@ -148,8 +153,8 @@ fn delete_book_by_isbn(req: &mut Request) -> IronResult<Response> {
 //list of students from request
 fn get_students(req: &mut Request) -> IronResult<Response> {
     let conn = req.get::<Write<DBConn, PostgresConnection>>().unwrap();
-    Ok(match req.extensions.find::<Router, Params>().unwrap().find("student") {
-        Some(student) => {
+    Ok(match req.extensions.find::<Router, Params>().unwrap().find("User") {
+        Some(User) => {
             let conn = conn.lock();
             let stmt = conn.prepare("SELECT * FROM users").unwrap();
             let mut users = Vec::new();
@@ -167,11 +172,12 @@ fn get_students(req: &mut Request) -> IronResult<Response> {
 //students from request
 fn get_student_by_name(req: &mut Request) -> IronResult<Response> {
     let conn = req.get::<Write<DBConn, PostgresConnection>>().unwrap();
-    Ok(match req.extensions.find::<Router, Params>().unwrap().find("student") {
-        Some(student) => {
+    Ok(match req.extensions.find::<Router, Params>().unwrap().find("User"){
+        Some(User) => {
             let conn = conn.lock();
-            let stmt = conn.prepare("SELECT * FROM users WHERE student = $1").unwrap();
-            for row in stmt.query(&[&String::from_str(student)]).unwrap() {
+            let stmt = conn.prepare("SELECT * FROM users WHERE name=$1 OR email=$2 OR student_id=$3").unwrap();
+            
+            for row in stmt.query(&[&String::from_str(User.name),&String::from_str(User.email),&String::from_str(User.student_id)]).unwrap() {
                 let student = book_from_row(row);
                 return Ok(good(&student))
             }
@@ -186,11 +192,12 @@ fn get_student_by_name(req: &mut Request) -> IronResult<Response> {
 //update student from request
 fn update_student_by_name(req: &mut Request) -> IronResult<Response> {
     let conn = req.get::<Write<DBConn, PostgresConnection>>().unwrap();
-    Ok(match req.extensions.find::<Router, Params>().unwrap().find("student") {
-        Some(student) => {
+    Ok(match req.extensions.find::<Router, Params>().unwrap().find("User") {
+        Some(User) => {
             let conn = conn.lock();
-            let stmt = conn.prepare("UPDATE users SET student=$1,student=$2,student=$3,student=$4 WHERE student=$5").unwrap();
-            match stmt.execute(&[&String::from_str(student),&String::from_str(student),&String::from_str(student),&student,&student]) {
+            let stmt = conn.prepare("UPDATE users SET name=$1,email=$2,student_id=$3,permission=$4 WHERE name=$1 OR email=$2 OR student_id=$3").unwrap();
+            
+            match stmt.execute(&[&String::from_str(User.name),&String::from_str(User.email),&String::from_str(User.student_id)]) {
     					Ok(num) => println!("Update Student! {}", num),
     					Err(err) => println!("Error executing update_student_by_name: {}", err)
 						}
@@ -203,11 +210,12 @@ fn update_student_by_name(req: &mut Request) -> IronResult<Response> {
 //add student from request
 fn add_student_by_name(req: &mut Request) -> IronResult<Response> {
     let conn = req.get::<Write<DBConn, PostgresConnection>>().unwrap();
-    Ok(match req.extensions.find::<Router, Params>().unwrap().find("student") {
-        Some(student) => {
+    Ok(match req.extensions.find::<Router, Params>().unwrap().find("User") {
+        Some(User) => {
             let conn = conn.lock();
-            let stmt = conn.prepare("INSERT INTO users WHERE student=$1").unwrap();
-            match stmt.execute(&[&String::from_str(student),&String::from_str(student),&String::from_str(student),&student]) {
+            let stmt = conn.prepare("INSERT INTO users (name,email,student_id,permission) WHERE ($1,$2,$3,$4)").unwrap();
+            
+            match stmt.execute(&[&String::from_str(User.name),&String::from_str(User.email),&String::from_str(User.student_id)]) {
     					Ok(num) => println!("Added Student! {}", num),
     					Err(err) => println!("Error executing add_student_by_name: {}", err)
 						}
@@ -220,11 +228,12 @@ fn add_student_by_name(req: &mut Request) -> IronResult<Response> {
 //delete student from request
 fn delete_student_by_name(req: &mut Request) -> IronResult<Response> {
     let conn = req.get::<Write<DBConn, PostgresConnection>>().unwrap();
-    Ok(match req.extensions.find::<Router, Params>().unwrap().find("student") {
-        Some(student) => {
+    Ok(match req.extensions.find::<Router, Params>().unwrap().find("User") {
+        Some(User) => {
             let conn = conn.lock();
-            let stmt = conn.prepare("DELETE from users WHERE student=$1").unwrap();
-            match stmt.execute(&[&String::from_str(student)]) {
+            let stmt = conn.prepare("DELETE from users WHERE name=$1 OR email=$2 OR student_id=$3").unwrap();
+            
+            match stmt.execute(&[&String::from_str(User.name),&String::from_str(User.email),&String::from_str(User.student_id)]) {
     					Ok(num) => println!("Deleted Student! {}", num),
     					Err(err) => println!("Error executing delete_student_by_name: {}", err)
 						}
@@ -267,15 +276,15 @@ fn main() {
   router.put("/book/:isbn", update_book_by_isbn);
 
   //get student 
-  router.get("/student", get_student_by_name);
+  router.get("/student:name", get_student_by_name);
   //get list of students
   router.get("/student", get_students);
   //add student from name
-  router.post("/student", add_student_by_name);
+  router.post("/student:name", add_student_by_name);
   //delete students from name
-  router.delete("/student", delete_student_by_name);
+  router.delete("/student:name", delete_student_by_name);
   //update student from name
-  router.put("/student", update_student_by_name);
+  router.put("/student:name", update_student_by_name);
 
   //manages the request through IRON Middleware web framework
   let mut chain = ChainBuilder::new(router);
